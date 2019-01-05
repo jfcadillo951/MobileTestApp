6ba949685206d0cf2225d2ae3a2e236a01f6b933
Franco <jfcadillo951@gmail.com>
2019-01-05 01:12:18 -0300

    first commit

---
+++ MobileTestApp/MobileTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileTestApp
+//
+//  Created by Nisum on 1/5/19.
+//  Copyright © 2019 Demo1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
